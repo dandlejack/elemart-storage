032fb060490d33a606554219f270fd513bcd1089
dandlejack <dandlejack@gmail.com>
2021-05-24 14:32:01 +0700

    fix redirect to / only

--- src/components/RouteComponent/PrivateRoute.tsx
+++ src/components/RouteComponent/PrivateRoute.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Redirect,Route} from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import { useAuth } from '../Auth/auth-context';
-
-export const PrivateRoute = ({component : Component, ...rest}:any) => {
-    const {myAccount} = useAuth()
+import Cookie from 'js-cookie'
+export const PrivateRoute = ({ component: Component, ...rest }: any) => {
+    const { myAccount } = useAuth()
     const cpAccount = JSON.parse(JSON.stringify(myAccount))
-    return(
+    const getCookie = Cookie.get('profile')
+
+    return (
         <Route {...rest} render={routeProps => (
-            cpAccount.login ? <Component {...routeProps}/> : <Redirect to='/login'/>
-        )} />
+            cpAccount.login || getCookie !== undefined ? <Component {...routeProps} /> : <Redirect to='/login' />
+        )
+        } />
     )
 }
